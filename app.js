642fa6da44946ffadf020b64afadd8c6ec225e43
bebinh5963 <thichche12@gmail.com>
2026-07-15 23:36:04 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route"); // Nạp tờ menu route vào đây
+
 const app = express();
 
 app.use(cors());
@@ -8,5 +10,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+// Đăng ký sử dụng menu route. Tất cả các đường dẫn trong contact.route.js 
+// khi chạy thực tế sẽ phải bắt đầu bằng "/api/contacts"
+app.use("/api/contacts", contactsRouter); 
 
 module.exports = app;
